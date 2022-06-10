b23f40d1442353ac495efdf8df34ee8de54d9dab
joparino <nikstrozzz@gmail.com>
2022-06-10 23:40:51 +0700

    db models

---
+++ entities/models.h
@@ -0,0 +1,124 @@
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <QMetaType>
+
+namespace jp
+{
+    struct Author
+    {
+        std::string name;
+
+        friend bool operator==(const std::shared_ptr<jp::Author>& lhs, const std::string& rhs)
+        {
+            return lhs->name == rhs;
+        }
+    };
+
+
+    struct Genre
+    {
+        std::string name;
+
+        friend bool operator==(const std::shared_ptr<jp::Genre>& lhs, const std::string& rhs)
+        {
+            return lhs->name == rhs;
+        }
+    };
+
+
+    struct Publisher
+    {
+        std::string name;
+
+        friend bool operator==(const std::shared_ptr<jp::Publisher>& lhs, const std::string& rhs)
+        {
+            return lhs->name == rhs;
+        }
+    };
+
+
+    struct Book
+    {
+        int id;
+        std::string title;
+        std::shared_ptr<jp::Author> author;
+        std::shared_ptr<jp::Genre> genre;
+        std::shared_ptr<jp::Publisher> publisher;
+        std::string year;
+        float price;
+        int count;
+
+        friend bool operator==(const std::shared_ptr<jp::Book>& lhs, const std::string& rhs)
+        {
+            return lhs->title == rhs;
+        }
+        friend bool operator==(const std::shared_ptr<jp::Book>& lhs, const int& rhs)
+        {
+            return lhs->id == rhs;
+        }
+    };
+
+
+    struct Client
+    {
+        int id;
+        std::string name;
+        std::string phoneNumber;
+
+        friend bool operator==(const std::shared_ptr<jp::Client>& lhs, const std::string& rhs)
+        {
+            return lhs->phoneNumber == rhs;
+        }
+    };
+
+
+    struct Status
+    {
+        int id;
+        std::string name;
+
+        friend bool operator==(const std::shared_ptr<jp::Status>& lhs, const std::string& rhs)
+        {
+            return lhs->name == rhs;
+        }
+        friend bool operator==(const std::shared_ptr<jp::Status>& lhs, const int& rhs)
+        {
+            return lhs->id == rhs;
+        }
+    };
+
+
+    struct Employee
+    {
+        int id;
+    };
+
+
+    struct Order
+    {
+        int id;
+        std::list<Book> books;
+        int employee;
+        std::shared_ptr<jp::Client> client;
+        std::shared_ptr<jp::Status> status;
+        std::string address;
+        std::string date;
+        float sum;
+
+        friend bool operator==(const std::shared_ptr<jp::Order>& lhs, const std::string& rhs)
+        {
+            return lhs->client->name == rhs;
+        }
+        friend bool operator==(const std::shared_ptr<jp::Order>& lhs, const int& rhs)
+        {
+            return lhs->id == rhs;
+        }
+    };
+}
+
+Q_DECLARE_METATYPE(std::shared_ptr<jp::Book>);
+Q_DECLARE_METATYPE(jp::Book);
+Q_DECLARE_METATYPE(std::shared_ptr<jp::Order>);
